2dabd0530d7029551bbc2a6a73671aa44edf20ba
Your Name <you@example.com>
2025-09-19 18:15:07 -0300

    Add file message-generator.js, adds variables for forming phrases

---
+++ codecademy/5/message-generator.js
@@ -0,0 +1,30 @@
+const subjects = [
+  "A flying toaster",
+  "My imaginary goldfish",
+  "The confused astronaut",
+  "A giant pancake",
+  "Your neighbor's refrigerator",
+  "The invisible duck",
+  "An angry pineapple"
+];
+
+const verbs = [
+  "dances with",
+  "screams at",
+  "secretly admires",
+  "is running away from",
+  "tries to explain quantum physics to",
+  "throws glitter on",
+  "can’t stop thinking about"
+];
+
+const complements = [
+  "a potato wearing sunglasses",
+  "the last slice of pizza",
+  "a very suspicious cat",
+  "a karaoke machine in space",
+  "a wormhole full of spaghetti",
+  "a rubber chicken army",
+  "a confused time traveler"
+];
+
